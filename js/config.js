5362bb3e5d0773986b3df0698dafe59f0f3e326c
takuboth <tky.takuboth@gmail.com>
2026-02-24 02:25:16 +0900

    Create config.js with game configuration constants
    Added configuration constants for game settings, roles, phases, and marks.

---
+++ js/config.js
@@ -0,0 +1,32 @@
+export const CONFIG = {
+  humanPlayerId: 0,
+  autoPlayers: true,
+  autoSafetySteps: 800,
+  effectsEnabled: true,
+};
+
+export const ROLES = {
+  WOLF: "人狼",
+  MAD: "狂人",
+  SEER: "占い",
+  GUARD: "狩人",
+  MEDIUM: "霊媒",
+  VILLAGER: "村人",
+};
+
+export const PHASES = {
+  ROUND0_MAD: "ROUND0_MAD",
+  ROUND0_GUARD: "ROUND0_GUARD",
+  SEER: "SEER",
+  LYNCH: "LYNCH",
+  MAD: "MAD",
+  GUARD: "GUARD",
+  BITE: "BITE",
+  END: "END",
+};
+
+export const MARK = {
+  GRAY: "GRAY",
+  WHITE: "WHITE",
+  BLACK: "BLACK",
+};
