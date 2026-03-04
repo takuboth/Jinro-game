6822bebd915362f71d825be1b2da8b4ce3280e64
takuboth <tky.takuboth@gmail.com>
2026-03-05 03:28:48 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,31 +1,33 @@
-export const CONFIG = {
-  humanPlayerId: 0,
-  autoPlayers: true,
-  autoSafetySteps: 800,
-};
-
 export const ROLES = {
-  WOLF: "人狼",
-  MAD: "狂人",
-  SEER: "占い",
-  GUARD: "狩人",
-  MEDIUM: "霊媒",
-  VILLAGER: "村人",
-};
-
-export const PHASES = {
-  ROUND0_MAD: "ROUND0：狂人",
-  ROUND0_GUARD: "ROUND0：狩人",
-  SEER: "占い",
-  LYNCH: "吊り",
-  MAD: "狂人",
-  GUARD: "狩人",
-  BITE: "噛み",
-  END: "終了",
+  WOLF: "WOLF",
+  MAD: "MAD",
+  SEER: "SEER",
+  GUARD: "GUARD",
+  MEDIUM: "MEDIUM",
+  VILLAGER: "VILLAGER",
 };
 
 export const MARK = {
   GRAY: "GRAY",
   WHITE: "WHITE",
   BLACK: "BLACK",
+};
+
+export const PHASES = {
+  ROUND0_MAD: "ROUND0_MAD",
+  ROUND0_GUARD: "ROUND0_GUARD",
+
+  SEER: "SEER",
+  LYNCH: "LYNCH",
+  MAD: "MAD",
+  GUARD: "GUARD",
+  BITE: "BITE",
+
+  END: "END",
+};
+
+export const CONFIG = {
+  humanPlayerId: 0,
+  autoPlayers: true,
+  autoSafetySteps: 999,
 };
